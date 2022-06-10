24420f65111f54b136b527f266a3a404ef59ed4e
elfsvet <stepanmatysik@gmail.com>
2022-06-09 22:04:34 -0400

    forgot to add test

--- __tests__/helpers.test.js
+++ __tests__/helpers.test.js
@@ -1,5 +1,4 @@
-const { format_date, format_plural } = require('../utils/helpers');
-
+const { format_date, format_plural, format_url } = require('../utils/helpers');
 
 
 test('format_date() returns a date string', () => {
@@ -8,9 +7,21 @@ test('format_date() returns a date string', () => {
     expect(format_date(date)).toBe('3/20/2022');
 });
 
-test('format_plural() returns a pluralized word',()=> {
-    expected(format_plural("tiger", 2)).toBe("tigers");
+test('format_plural() returns a pluralized word', () => {
+    const plural = format_plural("tiger", 2);
+    const singular = format_plural("lion", 1);
 
-    expected(format_plural("lion", 1)).toBe("lion");
-    
+    expect(plural).toBe("tigers");
+    expect(singular).toBe("lion");
+
+});
+
+test('format_url() returns a simplified url string', () => {
+    const url1 = format_url('http://test.com/page/1');
+    const url2 = format_url('https://www.coolstuff.com/abcdefg/');
+    const url3 = format_url('https://www.google.com?q=hello');
+
+    expect(url1).toBe('test.com');
+    expect(url2).toBe('coolstuff.com');
+    expect(url3).toBe('google.com');
 });
