7b01eebc28dd7ebcce751cce3411ce113dbdca7e
elfsvet <stepanmatysik@gmail.com>
2022-06-11 10:24:12 -0400

    finished 14.5.6

--- public/javascript/delete-post.js
+++ public/javascript/delete-post.js
@@ -1,10 +1,12 @@
 async function deleteHandler(event) {
     event.preventDefault();
+    // capture the id of post in the url cutting it in parts.
     const id = window.location.toString().split('/')[window.location.toString().split('/').length - 1];
     const response = await fetch(`/api/posts/${id}`, {
         method: 'DELETE'
     });
     if (response.ok) {
+        // redirect user to dashboard if success
         document.location.replace('/dashboard/');
     }
     else {
--- public/javascript/edit-post.js
+++ public/javascript/edit-post.js
@@ -1,13 +1,15 @@
 async function editFormHandler(event) {
     event.preventDefault();
-  
+  // capturing the post-title value from the form
     const title = document.querySelector('input[name="post-title"]').value.trim();
+    // getting id of post we are editing
     const id = window.location.toString().split('/')[
       window.location.toString().split('/').length - 1
     ];
     const response = await fetch(`/api/posts/${id}`, {
       method: 'PUT',
       body: JSON.stringify({
+        // part of the body to provide information 
         title
       }),
       headers: {
