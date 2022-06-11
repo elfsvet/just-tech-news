9a8dd85d206dd5de387b9cf6dccef6b2cb96ebd7
elfsvet <stepanmatysik@gmail.com>
2022-06-11 10:58:08 -0400

    capital

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -6,7 +6,7 @@ async function loginFormHandler(event) {
 
   if (email && password) {
     const response = await fetch('/api/users/login', {
-      method: 'POST',
+      method: 'post',
       body: JSON.stringify({
         email,
         password
@@ -31,7 +31,7 @@ async function signupFormHandler(event) {
 
   if (username && email && password) {
     const response = await fetch('/api/users', {
-      method: 'POST',
+      method: 'post',
       body: JSON.stringify({
         username,
         email,
--- server.js
+++ server.js
@@ -9,7 +9,6 @@ const hbs = exphbs.create({helpers});
 const app = express();
 const PORT = process.env.PORT || 3001;
 const session = require('express-session');
-const { use } = require('./controllers');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
 const sess = {
